6f3ff1997e003d28b14b995bf8381f3b0a3b06da
lehuynhphat2808 <52100772@student.tdtu.edu.vn>
2024-10-31 22:51:59 +0700

    feat: final

--- lib/todo.dart
+++ lib/todo.dart
@@ -3,4 +3,15 @@ class Todo {
   bool isComplete;
 
   Todo({required this.title, this.isComplete = false});
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(title: json['title'], isComplete: json['isComplete']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'isComplete': isComplete,
+    };
+  }
 }
--- lib/todo_provider.dart
+++ lib/todo_provider.dart
@@ -1,22 +1,54 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/todo.dart';
 
 class TodoProvider with ChangeNotifier {
   List<Todo> _todos = [];
   List<Todo> get todos => _todos;
 
+  TodoProvider() {
+    _loadTodos();
+  }
+
+  Future<void> _loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? todoString = prefs.getString('todos');
+    if (todoString != null) {
+      Iterable decoded = jsonDecode(todoString);
+      _todos = List<Todo>.from(decoded.map(
+        (e) => Todo.fromJson(e),
+      ));
+      notifyListeners();
+    }
+  }
+
+  Future<void> _saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String todosString = jsonEncode(_todos
+        .map(
+          (e) => e.toJson(),
+        )
+        .toList());
+    prefs.setString('todos', todosString);
+  }
+
   void addTodo(String title) {
     _todos.add(Todo(title: title));
+    _saveTodos();
     notifyListeners();
   }
 
   void removeTodo(int index) {
     _todos.removeAt(index);
+    _saveTodos();
     notifyListeners();
   }
 
   void toggleTodoStatus(int index) {
     _todos[index].isComplete = !todos[index].isComplete;
+    _saveTodos();
     notifyListeners();
   }
 }
